03a74a1f8b9d8ebb6d5ec5c2df990b05f2768c15
Alzidan <71370680+Alzidan@users.noreply.github.com>
2020-10-12 22:12:28 +0700

    Add files via upload

---
+++ ReturnValues1.java
@@ -0,0 +1,13 @@
+package com.PBOALZIDAN;
+
+public class ReturnValues1 {
+
+    static int myMethod(int x, int y){
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(10,5));
+    }
+
+}
---
+++ ReturnValues2.java
@@ -0,0 +1,14 @@
+package com.PBOALZIDAN;
+
+public class ReturnValues2 {
+
+    static int myMethod(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int z = myMethod(7, 7);
+        System.out.println(z);
+    }
+
+}
